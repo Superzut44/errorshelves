f00b698305f7ab685daaf12bcc44ededee033270
chavaudrey <chavaudreyxav@hotmail.fr>
2022-06-03 19:11:29 +0200

    components and services created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { ErrorListComponent } from './error-list/error-list.component';
+import { SingleErrorComponent } from './error-list/single-error/single-error.component';
+import { ErrorFormComponent } from './error-list/error-form/error-form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    ErrorListComponent,
+    SingleErrorComponent,
+    ErrorFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/error-list/single-error/single-error.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-error',
+  templateUrl: './single-error.component.html',
+  styleUrls: ['./single-error.component.scss']
+})
+export class SingleErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
