645d85b4bb9b019e5c242cb61a0047faff8a262d
chavaudrey <chavaudreyxav@hotmail.fr>
2022-06-04 15:48:35 +0200

    firebase integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'errorshelves';
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyB6jo62IcUsqIrGLfLlj3HCWqNeuRMnTd8",
+      authDomain: "errorshelves.firebaseapp.com",
+      projectId: "errorshelves",
+      storageBucket: "errorshelves.appspot.com",
+      messagingSenderId: "740667656459",
+      appId: "1:740667656459:web:ba9b29258b115f7644fa0e",
+      measurementId: "G-DBGTKH501C"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
