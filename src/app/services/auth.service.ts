279218fdb2b8a83855b4e474b6d29c726f70882b
chavaudrey <chavaudreyxav@hotmail.fr>
2022-06-07 17:33:57 +0200

    signIn

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-// v9 compat packages are API compatible with v8 code
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
